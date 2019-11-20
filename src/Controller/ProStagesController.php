c3d6e5055920147f9df54f3c64ad867e76db47d5
Kévin Oliva <olivakevin64@gmail.com>
2019-11-20 21:49:54 +0100

    Modification du controleur (routes) + Creation vues entreprises, formations et stages/id - TP1

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -7,13 +7,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStagesController extends AbstractController
 {
-    /**
-     * @Route("/pro/stages", name="pro_stages")
-     */
-    public function index()
-    {
-        return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'ProStagesController',
-        ]);
-    }
+  /**
+   * @Route("/", name="accueil_pro_stages")
+   */
+  public function index()
+  {
+      return $this->render('pro_stages/index.html.twig', [
+          'controller_name' => 'ProStagesController',
+      ]);
+  }
+
+  /**
+   * @Route("/entreprises", name="entreprises_prostages")
+   */
+  public function entreprises()
+  {
+      return $this->render('pro_stages/entreprises.html.twig');
+  }
+
+  /**
+   * @Route("/formations", name="formations_prostages")
+   */
+  public function formations()
+  {
+      return $this->render('pro_stages/formations.html.twig');
+  }
+
+  /**
+   * @Route("/stages/{id}", name="stages_id_prostages")
+   */
+  public function stages_id($id)
+  {
+      return $this->render('pro_stages/stages_id.html.twig',
+    ['idRessource' => $id]);
+  }
 }
