a4ebb225ad9e0420619288c22d69bab3fdda6516
Kévin Oliva <olivakevin64@gmail.com>
2019-12-19 12:03:00 +0100

    Mise en place du jeu de données dans index.html.twig + redirection vers le stage grâce à l'id

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -4,41 +4,51 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Stage;
 
 class ProStagesController extends AbstractController
 {
   /**
-   * @Route("/", name="accueil_pro_stages")
-   */
+  * @Route("/", name="accueil_pro_stages")
+  */
   public function index()
   {
-      return $this->render('pro_stages/index.html.twig', [
-          'controller_name' => 'ProStagesController',
-      ]);
+    // Récupérer le repository de l'entité Stage
+    $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+    // Récupérer les stages enregistrés en BD
+    $stages = $repositoryStage->findAll();
+    // Envoyer les stages récupérés à la vue chargée de les afficher
+    return $this->render('pro_stages/index.html.twig', ['stages'=>$stages]);
   }
 
   /**
-   * @Route("/entreprises", name="entreprises_prostages")
-   */
+  * @Route("/entreprises", name="entreprises_prostages")
+  */
   public function entreprises()
   {
-      return $this->render('pro_stages/entreprises.html.twig');
+    return $this->render('pro_stages/entreprises.html.twig');
   }
 
   /**
-   * @Route("/formations", name="formations_prostages")
-   */
+  * @Route("/formations", name="formations_prostages")
+  */
   public function formations()
   {
-      return $this->render('pro_stages/formations.html.twig');
+    return $this->render('pro_stages/formations.html.twig');
   }
 
   /**
-   * @Route("/stages/{id}", name="stages_id_prostages")
-   */
+  * @Route("/stages/{id}", name="stages_id_prostages")
+  */
   public function stages_id($id)
   {
-      return $this->render('pro_stages/stages_id.html.twig',
-    ['idRessource' => $id]);
+    // Récupérer le repository de l'entité Stage
+    $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+    // Récupérer les stages enregistrés en BD
+    $stage = $repositoryStage->find($id);
+    // Envoyer les stages récupérés à la vue chargée de les afficher
+
+    return $this->render('pro_stages/stages_id.html.twig',
+    ['stage' => $stage]);
   }
 }
