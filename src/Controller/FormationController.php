6c9c439f64b877c18c2c45379f1ee6e739210c29
Kévin Oliva <olivakevin64@gmail.com>
2020-03-10 14:54:18 +0100

    TP6 : CRUD

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Formation;
+use App\Entity\Stage;
+use App\Entity\Entreprise;
 use App\Form\FormationType;
 use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,84 +13,88 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/formation")
- */
+* @Route("/formation")
+*/
 class FormationController extends AbstractController
 {
-    /**
-     * @Route("/", name="formation_index", methods={"GET"})
-     */
-    public function index(FormationRepository $formationRepository): Response
-    {
-        return $this->render('formation/index.html.twig', [
-            'formations' => $formationRepository->findAll(),
-        ]);
+  /**
+  * @Route("/", name="formation_index", methods={"GET"})
+  */
+  public function index(FormationRepository $formationRepository): Response
+  {
+    return $this->render('formation/index.html.twig', [
+      'formations' => $formationRepository->findAll(),
+    ]);
+  }
+
+  /**
+  * @Route("/new", name="formation_new", methods={"GET","POST"})
+  */
+  public function new(Request $request): Response
+  {
+    $formation = new Formation();
+    $form = $this->createForm(FormationType::class, $formation);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($formation);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('formation_index');
     }
 
-    /**
-     * @Route("/new", name="formation_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $formation = new Formation();
-        $form = $this->createForm(FormationType::class, $formation);
-        $form->handleRequest($request);
+    return $this->render('formation/new.html.twig', [
+      'formation' => $formation,
+      'form' => $form->createView(),
+    ]);
+  }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($formation);
-            $entityManager->flush();
+  /**
+  * @Route("/{nom}", name="formation_show", methods={"GET"})
+  */
+  public function showByName(Formation $formation, $nom): Response
+  {
+    // Récupérer le repository de l'entité Entreprise
+    $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+    // Récupérer les entreprises enregistrées en BD
+    $stagesParNomFormation = $repositoryStage->findByNomFormation($nom);
+    // Envoyer les entreprises récupérées à la vue chargée de les afficher
+    return $this->render('formation/show.html.twig', ['stagesParNomFormation'=>$stagesParNomFormation, 'formation' => $formation]);
 
-            return $this->redirectToRoute('formation_index');
-        }
+  }
 
-        return $this->render('formation/new.html.twig', [
-            'formation' => $formation,
-            'form' => $form->createView(),
-        ]);
+  /**
+  * @Route("/{id}/edit", name="formation_edit", methods={"GET","POST"})
+  */
+  public function edit(Request $request, Formation $formation): Response
+  {
+    $form = $this->createForm(FormationType::class, $formation);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->getDoctrine()->getManager()->flush();
+
+      return $this->redirectToRoute('formation_index');
     }
 
-    /**
-     * @Route("/{id}", name="formation_show", methods={"GET"})
-     */
-    public function show(Formation $formation): Response
-    {
-        return $this->render('formation/show.html.twig', [
-            'formation' => $formation,
-        ]);
+    return $this->render('formation/edit.html.twig', [
+      'formation' => $formation,
+      'form' => $form->createView(),
+    ]);
+  }
+
+  /**
+  * @Route("/{id}", name="formation_delete", methods={"DELETE"})
+  */
+  public function delete(Request $request, Formation $formation): Response
+  {
+    if ($this->isCsrfTokenValid('delete'.$formation->getId(), $request->request->get('_token'))) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->remove($formation);
+      $entityManager->flush();
     }
 
-    /**
-     * @Route("/{id}/edit", name="formation_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Formation $formation): Response
-    {
-        $form = $this->createForm(FormationType::class, $formation);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('formation_index');
-        }
-
-        return $this->render('formation/edit.html.twig', [
-            'formation' => $formation,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="formation_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Formation $formation): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$formation->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($formation);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('formation_index');
-    }
+    return $this->redirectToRoute('formation_index');
+  }
 }
--- src/Entity/Stage.php
+++ src/Entity/Stage.php
@@ -110,4 +110,8 @@ class Stage
 
         return $this;
     }
+
+    public function __toString(){
+      return $this->getTitre();
+    }
 }
